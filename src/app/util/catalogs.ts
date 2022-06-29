3c80bbc413c18d0206e7a9964a2929018845ba84
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-06-29 10:53:15 -0500

    [Fix]: Catalogo general => Descarga de excel

--- src/app/util/catalogs.ts
+++ src/app/util/catalogs.ts
@@ -9,10 +9,10 @@ export const catalogs: IOptionsCatalog[] = [
   { value: "field", label: "Especialidad", type: "normal" },
   { value: "payment", label: "Forma de pago", type: "normal" },
   { value: "indicator", label: "Indicadores", type: "description" },
-  { value: "workList", label: "Listas de trabajo", type: "description" },
+  { value: "workList", label: "Listas de trabajo", type: "normal" },
   { value: "delivery", label: "Paqueterias", type: "normal" },
   { value: "method", label: "Métodos", type: "normal" },
-  { value: "paymentMethod", label: "Métodos de pago", type: "normal" },
+  { value: "paymentMethod", label: "Métodos de pago", type: "description" },
   { value: "sampleType", label: "Tipo de Muestra", type: "normal" },
   { value: "useOfCFDI", label: "Uso de CFDI", type: "description" },
 ];
