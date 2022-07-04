24a325bd2cf7d26999093bace981153aad85e9f4
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-07-04 17:14:27 -0500

    [Feat]: Solicitudes => Inicio de pestaña de caja

---
+++ src/app/common/form/proposal/NumberInput.tsx
@@ -0,0 +1,139 @@
+import { Form, FormItemProps, InputNumber, Space, Tooltip } from "antd";
+import { Rule } from "antd/lib/form";
+import React, { useEffect, useRef, useState } from "react";
+import { InfoCircleTwoTone } from "@ant-design/icons";
+
+interface IProps {
+  formProps: FormItemProps<any>;
+  max?: number;
+  min: number;
+  required?: boolean;
+  prefix?: React.ReactNode;
+  type?: "text" | "password" | "number";
+  placeholder?: string;
+  readonly?: boolean;
+  width?: string | number;
+  suffix?: React.ReactNode;
+  style?: React.CSSProperties;
+  showLabel?: boolean;
+  errors?: any[];
+}
+
+const NumberInput = ({
+  formProps: itemProps,
+  max,
+  min,
+  required,
+  placeholder,
+  readonly,
+  width,
+  suffix,
+  style,
+  showLabel: isGroup,
+  errors,
+}: IProps) => {
+  let ref = useRef<HTMLDivElement>(null);
+
+  const [paddingRight, setPaddingRight] = useState(7);
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      console.log(entries[0]);
+      setPaddingRight(entries[0].target.clientWidth);
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    const _ref = ref.current;
+
+    return () => {
+      if (_ref) observer.unobserve(_ref);
+    };
+  }, []);
+
+  let rules: Rule[] = [];
+
+  if (max) {
+    rules.push({
+      type: "number",
+      max,
+      message: `El máximo es de ${max}`,
+    });
+  }
+
+  rules.push({
+    type: "number",
+    min,
+    message: `El mínimo es de ${min}`,
+  });
+
+  if (required) {
+    rules.push({
+      required: true,
+      message: "El campo es requerido",
+    });
+  }
+
+  return (
+    <div className="custom-input">
+      <Form.Item
+        {...itemProps}
+        name={itemProps.name}
+        label={itemProps.label}
+        labelAlign={itemProps.labelAlign ?? "right"}
+        rules={rules}
+        help=""
+        className="no-error-text"
+      >
+        <InputNumber
+          // style={{ width: "100%" }}
+          // autoComplete={itemProps.name?.toString()}
+          autoComplete="off"
+          placeholder={placeholder ?? itemProps.label?.toString()}
+          disabled={readonly}
+          type={"number"}
+          style={{
+            paddingRight: paddingRight,
+            width: width ?? "100%",
+            ...(style ?? {}),
+          }}
+        />
+      </Form.Item>
+      {/* {(!!suffix || isGroup || !!errors) && ( */}
+      <div
+        className={`suffix-container ${readonly ? "disabled" : ""}`}
+        style={{ display: !!suffix || isGroup || !!errors ? "" : "none" }}
+        ref={ref}
+      >
+        <Space size="small">
+          {suffix ? suffix : null}
+          {isGroup ? (
+            <Tooltip key="info" title={itemProps.label}>
+              <InfoCircleTwoTone />
+            </Tooltip>
+          ) : null}
+          {errors && errors.length > 0 ? (
+            <Tooltip
+              key="error"
+              color="red"
+              title={
+                <ul style={{ listStyleType: "none", margin: 0, padding: 0 }}>
+                  {errors.map((x) => (
+                    <li key={x}>{x}</li>
+                  ))}
+                </ul>
+              }
+              style={{ display: "none" }}
+            >
+              <InfoCircleTwoTone twoToneColor={"red"} />
+            </Tooltip>
+          ) : null}
+        </Space>
+      </div>
+      {/* )} */}
+    </div>
+  );
+};
+
+export default NumberInput;
