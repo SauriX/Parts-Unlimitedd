1b286143cbb0be7f060bbfc1e989dfa3a484e864
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-09-27 10:20:17 -0500

    [Feat] Perfil => Validacion para comprobar usuario Administrador

--- src/app/api/profile.ts
+++ src/app/api/profile.ts
@@ -5,7 +5,10 @@ import requests from "./agent";
 const Profile = {
   getMenu: (): Promise<IMenu[]> => requests.get(`profile/menu`),
   getProfile: (): Promise<IProfile> => requests.get(`profile/me`),
-  login: (creds: ILoginForm): Promise<IProfile> => requests.post("profile/login", creds),
+  login: (creds: ILoginForm): Promise<IProfile> =>
+    requests.post("profile/login", creds),
+  validateAdmin: (creds: ILoginForm): Promise<boolean> =>
+    requests.post("profile/validateAdmin", creds),
 };
 
 export default Profile;
