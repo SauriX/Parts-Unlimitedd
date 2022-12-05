655a0bc171898f3356d936d5783523119360c63b
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-12-05 09:14:03 -0600

    [Feat] Solicitudes => Imprimir ticket por pago

--- src/components/request/detail/content/invoice/RequestInvoiceDetail.tsx
+++ src/components/request/detail/content/invoice/RequestInvoiceDetail.tsx
@@ -1,8 +1,9 @@
-import { Button, Checkbox, Col, Form, Row, Spin } from "antd";
+import { Button, Checkbox, Col, Form, Row, Space, Spin, Table } from "antd";
 import { observer } from "mobx-react-lite";
 import React, { useEffect, useState } from "react";
 import SelectInput from "../../../../../app/common/form/proposal/SelectInput";
 import TextInput from "../../../../../app/common/form/proposal/TextInput";
+import { IColumns } from "../../../../../app/common/table/utils";
 import {
   IRequestCheckIn,
   IRequestPayment,
@@ -19,6 +20,12 @@ interface IFormInvoice {
   numeroCuenta: string;
   configuracion: string[];
   metodoEnvio: string[];
+}
+
+interface IDetailInvoice {
+  clave: string;
+  descripcion: string;
+  importe: string;
 }
 
 const formItemLayout = {
@@ -51,7 +58,7 @@ const RequestInvoiceDetail = ({
   taxData,
 }: RequestInvoiceDetailProps) => {
   const { requestStore, optionStore, modalStore } = useStore();
-  const { checkInPayment } = requestStore;
+  const { studies, packs, totals, checkInPayment } = requestStore;
   const { paymentOptions, cfdiOptions, getPaymentOptions, getcfdiOptions } =
     optionStore;
   const { closeModal } = modalStore;
@@ -60,6 +67,29 @@ const RequestInvoiceDetail = ({
 
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState<IFormError[]>([]);
+  const [detailData, setDetailData] = useState<IDetailInvoice[]>([]);
+
+  const detailColumns: IColumns<IDetailInvoice> = [
+    {
+      dataIndex: "clave",
+      key: "clave",
+      title: "Clave",
+      width: "30%",
+    },
+    {
+      dataIndex: "descripcion",
+      key: "descripcion",
+      title: "Descripción",
+      width: "50%",
+    },
+    {
+      dataIndex: "importe",
+      key: "importe",
+      title: "Importe",
+      width: "20%",
+      align: "right",
+    },
+  ];
 
   useEffect(() => {
     getPaymentOptions();
@@ -105,89 +135,121 @@ const RequestInvoiceDetail = ({
     );
   }, [form, payments]);
 
+  useEffect(() => {
+    const totalPayment = payments.reduce((acc, obj) => acc + obj.cantidad, 0);
+    if (totalPayment === totals.total) {
+      setDetailData([
+        ...studies.map((x) => ({
+          clave: x.clave,
+          descripcion: x.nombre,
+          importe: moneyFormatter.format(x.precioFinal),
+        })),
+      ]);
+    } else {
+      setDetailData([
+        {
+          clave: taxData.rfc,
+          descripcion: `Pago por ${moneyFormatter.format(totalPayment)}`,
+          importe: moneyFormatter.format(totalPayment),
+        },
+      ]);
+    }
+  }, []);
+
   return (
     <Spin spinning={loading}>
-      <Form<IFormInvoice>
-        {...formItemLayout}
-        form={form}
-        name="invoice"
-        onFinish={onFinish}
-        onFinishFailed={({ errorFields }) => {
-          const errors = errorFields.map((x) => ({
-            name: x.name[0].toString(),
-            errors: x.errors,
-          }));
-          setErrors(errors);
-        }}
-        scrollToFirstError
-      >
-        <Row gutter={[0, 12]}>
-          <Col span={12}>
-            <SelectInput
-              formProps={{
-                name: "usoCfdi",
-                label: "Uso de CFDI",
-              }}
-              options={cfdiOptions}
-              required
-              errors={errors.find((x) => x.name === "usoCfdi")?.errors}
-            />
-          </Col>
-          <Col span={7}></Col>
-          <Col span={5}>
-            <TextInput
-              formProps={{
-                name: "cantidad",
-                label: "Cantidad",
-                labelCol: { span: 12 },
-                wrapperCol: { span: 12 },
-              }}
-              readonly
-            />
-          </Col>
-          <Col span={12}>
-            <TextInput
-              formProps={{
-                name: "numeroCuenta",
-                label: "Número de cuenta",
-              }}
-              required
-              errors={errors.find((x) => x.name === "numeroCuenta")?.errors}
-            />
-          </Col>
-          {/* <Col span={9}>
+      <Space direction="vertical">
+        <Form<IFormInvoice>
+          {...formItemLayout}
+          form={form}
+          name="invoice"
+          onFinish={onFinish}
+          onFinishFailed={({ errorFields }) => {
+            const errors = errorFields.map((x) => ({
+              name: x.name[0].toString(),
+              errors: x.errors,
+            }));
+            setErrors(errors);
+          }}
+          scrollToFirstError
+        >
+          <Row gutter={[0, 12]}>
+            <Col span={12}>
+              <SelectInput
+                formProps={{
+                  name: "usoCfdi",
+                  label: "Uso de CFDI",
+                }}
+                options={cfdiOptions}
+                required
+                errors={errors.find((x) => x.name === "usoCfdi")?.errors}
+              />
+            </Col>
+            <Col span={7}></Col>
+            <Col span={5}>
+              <TextInput
+                formProps={{
+                  name: "cantidad",
+                  label: "Cantidad",
+                  labelCol: { span: 12 },
+                  wrapperCol: { span: 12 },
+                }}
+                readonly
+              />
+            </Col>
+            <Col span={12}>
+              <TextInput
+                formProps={{
+                  name: "numeroCuenta",
+                  label: "Número de cuenta",
+                }}
+                required
+                errors={errors.find((x) => x.name === "numeroCuenta")?.errors}
+              />
+            </Col>
+            {/* <Col span={9}>
             <SelectInput
               formProps={{ name: "serieCfdi", label: "Serie CFDI" }}
               options={[]}
             />
           </Col> */}
-          <Col span={24} style={{ textAlign: "start" }}>
-            <Form.Item
-              noStyle
-              name="configuracion"
-              labelCol={{ span: 0 }}
-              wrapperCol={{ span: 24 }}
-            >
-              <Checkbox.Group options={settingsOptions} />
-            </Form.Item>
-          </Col>
-          <Col span={18} style={{ textAlign: "start" }}>
-            <Form.Item
-              noStyle
-              name="metodoEnvio"
-              labelCol={{ span: 0 }}
-              wrapperCol={{ span: 24 }}
-            >
-              <Checkbox.Group options={sendOptions} />
-            </Form.Item>
-          </Col>
-          <Col span={6} style={{ textAlign: "end" }}>
-            <Button type="primary" htmlType="submit">
-              Registrar
-            </Button>
-          </Col>
-        </Row>
-      </Form>
+            <Col span={24} style={{ textAlign: "start" }}>
+              <Form.Item
+                noStyle
+                name="configuracion"
+                labelCol={{ span: 0 }}
+                wrapperCol={{ span: 24 }}
+              >
+                <Checkbox.Group options={settingsOptions} />
+              </Form.Item>
+            </Col>
+            <Col span={18} style={{ textAlign: "start" }}>
+              <Form.Item
+                noStyle
+                name="metodoEnvio"
+                labelCol={{ span: 0 }}
+                wrapperCol={{ span: 24 }}
+              >
+                <Checkbox.Group options={sendOptions} />
+              </Form.Item>
+            </Col>
+            <Col span={6} style={{ textAlign: "end" }}>
+              <Button type="primary" htmlType="submit">
+                Registrar
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        <Table<IDetailInvoice>
+          key="clave"
+          title={() => "Detalle"}
+          columns={detailColumns}
+          dataSource={detailData}
+          pagination={false}
+          sticky
+          scroll={{ y: 400 }}
+        />
+      </Space>
     </Spin>
   );
 };
