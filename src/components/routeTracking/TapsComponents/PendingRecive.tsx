b0c2ba52ae68d2a49774ee499fe8769fafbfde16
julio sauri <jcastro@Axsistec.com>
2023-01-11 16:31:12 -0500

    [fix] label sucursal

--- src/components/routeTracking/TapsComponents/PendingRecive.tsx
+++ src/components/routeTracking/TapsComponents/PendingRecive.tsx
@@ -527,8 +527,8 @@ const PendingRecive = () => {
               formProps={{ name: "fecha", label: "Fecha" }}
             ></DateInput>
           </Col>
-          <Col span={2}></Col>
-          <Col span={4}>
+
+          <Col span={6}>
             <SelectInput
             form={form}
               formProps={{
